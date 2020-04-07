b27af6642fe59f7bf10f9ae74b38598030fcae94
Aman941 <aman.verma9412345@gmail.com>
2020-04-07 16:13:00 +0530

    first commit

---
+++ precache-manifest.d912d10b144b442be09d916049c3a3c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b53d0c16bdc03e93b057fbb31b75b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1db6afc8d4f7d2c1db2c",
+    "url": "/static/css/main.73710fac.chunk.css"
+  },
+  {
+    "revision": "fd917c22cf81e7ab0f3c",
+    "url": "/static/js/2.17db6e64.chunk.js"
+  },
+  {
+    "revision": "68d7f76f94070edf8ae0d7dcf88f80d6",
+    "url": "/static/js/2.17db6e64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db6afc8d4f7d2c1db2c",
+    "url": "/static/js/main.6cd92cb4.chunk.js"
+  },
+  {
+    "revision": "d5a4674d5b6d3d6954a5",
+    "url": "/static/js/runtime-main.81fc0c82.js"
+  }
+]);
